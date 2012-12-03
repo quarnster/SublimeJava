e8e7441758f86f556060d1a07913271e8821834a
Philip Oliver-Paull <oliverpa@mailbox.sc.edu>
2012-12-03 10:08:30 -0500

    pep8 formatting

--- classopener.py
+++ classopener.py
@@ -7,6 +7,9 @@ remove_dollar = lambda classname: classname.replace('$$', '.')
 
 class JavaClassOpener(object):
 
+    MSG_NO_CLASSES_FOUND = \
+        "No classes could be found. Check your %s project setting."
+
     def __init__(self, completion, view, under_cursor, setting_name):
         self.completion = completion
         self.view = view
@@ -15,11 +18,15 @@ class JavaClassOpener(object):
         self.setting_name = setting_name
 
     def show(self):
-        classname = self.completion.get_class_under_cursor() if self.under_cursor else None
+        classname = self.completion.get_class_under_cursor() \
+            if self.under_cursor \
+            else None
 
         options = []
         for path in self.completion.get_setting(self.setting_name, ""):
-            path = os.path.abspath(self.completion.expand_path(path, self.window))
+            path = os.path.abspath(
+                self.completion.expand_path(path, self.window)
+            )
             options.extend(self._scan_dir(path, classname))
 
         def do_open(result):
@@ -32,7 +39,8 @@ class JavaClassOpener(object):
         elif len(options) > 1:
             self.window.show_quick_panel([t[0] for t in options], do_open)
         else:
-            self.completion.show_error("No classes could be found. Check your %s project setting." % self.setting_name)
+            self.completion.show_error(self.MSG_NO_CLASSES_FOUND %
+                                       self.setting_name)
 
     def _scan_dir(self, base, classname_to_find=None):
         return []
@@ -43,7 +51,10 @@ class JavaClassOpener(object):
 
 class JavaSourceOpener(JavaClassOpener):
     def __init__(self, completion, view, under_cursor):
-        super(JavaSourceOpener, self).__init__(completion, view, under_cursor, "sublimejava_srcpath")
+        super(JavaSourceOpener, self).__init__(completion,
+                                               view,
+                                               under_cursor,
+                                               "sublimejava_srcpath")
 
     def _scan_dir(self, base, classname_to_find=None):
         for root_name, dir_names, filenames in os.walk(base):
@@ -56,7 +67,8 @@ class JavaSourceOpener(JavaClassOpener):
                 if not filename.endswith(".java"):
                     continue
                 classname = package + filename.split('.')[0]
-                if classname_to_find is not None and classname != classname_to_find:
+                if (classname_to_find is not None and
+                        classname != classname_to_find):
                     continue
                 yield classname, (root_name + "/" + filename)
 
@@ -66,10 +78,15 @@ class JavaSourceOpener(JavaClassOpener):
 
 class JavaDocOpener(JavaClassOpener):
     def __init__(self, completion, view, under_cursor):
-        super(JavaDocOpener, self).__init__(completion, view, under_cursor, "sublimejava_docpath")
+        super(JavaDocOpener, self).__init__(completion,
+                                            view,
+                                            under_cursor,
+                                            "sublimejava_docpath")
 
     def _scan_dir(self, base, classname_to_find=None):
-        search_classname = None if classname_to_find is None else remove_dollar(classname_to_find)
+        search_classname = None \
+            if classname_to_find is None \
+            else remove_dollar(classname_to_find)
 
         for root_name, dir_names, filenames in os.walk(base):
             try:
@@ -79,10 +96,13 @@ class JavaDocOpener(JavaClassOpener):
             package = path_to_full(root_name[len(base) + 1:]) + "."
             for filename in filenames:
                 # - gets all the java overview bidness
-                if not filename.endswith('.html') or '-' in filename or filename == 'index.html':
+                if (not filename.endswith('.html') or
+                        '-' in filename or
+                        filename == 'index.html'):
                     continue
                 classname = remove_dollar(package + filename[:-5])
-                if search_classname is not None and classname != search_classname:
+                if (search_classname is not None and
+                        classname != search_classname):
                     continue
                 yield classname, (root_name + "/" + filename)
 
